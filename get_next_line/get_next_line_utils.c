aa6e1aacf20f3e460b3e48eb6bb2687f3bbec794
GinaldoAORUS <gnaldinho79@gmail.com>
2025-04-23 21:21:11 +0100

    add utils

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-int	strlen(char *str)
+size_t	ft_strlen(const char *str)
 {
 	int	i;
 
@@ -8,4 +8,65 @@ int	strlen(char *str)
 	while (!str)
 		i++;
 	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	int		i;
+
+	i = (int)ft_strlen(s);
+	str = (char *)malloc(i + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	rlen;
+
+	if (!s)
+		return (NULL);
+	rlen = ft_strlen(s);
+	if (start >= rlen)
+		return (ft_strdup(""));
+	rlen -= start;
+	if (rlen > len)
+		rlen = len;
+	str = (char *)malloc(rlen + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
