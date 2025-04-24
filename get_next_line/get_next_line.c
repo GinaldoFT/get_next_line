dc1431eebaa3be1ba29c16bee3e6e8c5597ac78e
ginfranc <ginfranc@student.42porto.com>
2025-04-24 11:57:29 +0100

    add .c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,5 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ginfranc <ginfranc@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 10:35:02 by ginfranc          #+#    #+#             */
+/*   Updated: 2025/04/24 10:36:05 by ginfranc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+	ssize_t		bytes_read;
+	int		i;
+
+	i = 0;
+	line = malloc(BUFFER_SIZE + 1);
+	if (!line)
+        return (NULL);
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	while (i < bytes_read && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+
+	if (buffer[i] == '\n')
+		line[i++] = '\n';
+
+	line[i] = '\0';
+	return (line);
 }
+
+#include <stdio.h>
+
+int	main(int ac, char *av[])
+{
+	int	fd;
+	char	*text;
+	int	n;
+
+	if (ac != 2)
+		return (1);
+	n = 4;
+	fd = (int)open(av[1], O_RDONLY);
+	while (--n > 0)
+	{
+		text = get_next_line(fd);
+		printf("%s", text);
+		free(text);
+	}
+	return (0);
+}
+
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ginfranc <ginfranc@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 10:36:44 by ginfranc          #+#    #+#             */
+/*   Updated: 2025/04/24 10:37:01 by ginfranc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 1
@@ -14,5 +27,6 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_line(char *str);
 
 #endif
