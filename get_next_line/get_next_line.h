3b21e79ab328a421360bffa85cb75ad8341ee364
Ginaldo Araújo <gnaldinho79@gmail.com>
2025-05-02 09:09:41 +0100

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -26,6 +26,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strdup(const char *s);
 
 #endif
